1439736e7580e1a7cc3081b06677312e62fe5299
gerg <gregross.dev@gmail.com>
2021-02-27 17:30:37 -0500

    styled-components

---
+++ src/themes/theme.js
@@ -0,0 +1,35 @@
+export default {
+  fonts: {
+    main: "Muli, sans-serif",
+    code: "Roboto Mono, monospace",
+  },
+  colors: {
+    main1: "hsl(207,70%,59%)",
+    main2: "hsl(207,70%,94%)",
+    dark1: "hsl(227,02%,12%)",
+    dark2: "hsl(227,02%,26%)",
+    dark3: "hsl(227,02%,64%)",
+    light1: "hsl(00,00%,97%)",
+    light2: "hsl(00,00%,99%)",        
+  },
+  breakpoints: {
+    tablet: "only screen and (min-width: 600px)",
+    desktop: "only screen and (min-width: 900px)",  
+  },
+  spacings: {
+    xxSmall: ".25rem",
+    xSmall: ".50rem",
+    small: "1rem",
+    medium: "2rem",
+    large: "3rem",
+    xLarge: "4rem",
+    xxLarge: "6rem", 
+  },
+  animations: {
+    button: "box-shadow 0.3s ease",
+    link: "color 0.2s ease",
+  },
+  shadows: {
+    shadow1: "0px 5px 20px rgba(30, 30, 31, 0.05)",
+  },
+}

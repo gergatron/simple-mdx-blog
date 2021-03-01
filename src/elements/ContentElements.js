aa6b6763406a572487b0c3eddf60f3ae0ac64165
gergatron <47074718+gergatron@users.noreply.github.com>
2021-03-01 17:49:47 -0500

    Content

---
+++ src/elements/ContentElements.js
@@ -0,0 +1,17 @@
+import style from "styled-components"
+
+export const ContentWrapper = styled.main`
+  grid-column: 2 / span 6;
+  grid-row: 3 / span 5;
+  background: ${props => props.theme.colors.light2};
+  padding: ${props =>
+    `${props.theme.spacings.medium} ${props.theme.spacings.large}`};
+  box-shadow: ${props => props.theme.shadows.shadow1};
+  z-index: 10;
+
+  @media ${props => props.theme.breakpoints.desktop} {
+    padding: ${props =>
+      `${props.theme.spacings.xLarge} ${props.theme.spacings.xxLarge}`};
+    grid-column: 4 / span 8;
+  }
+`

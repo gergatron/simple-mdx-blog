967db3c91ee34de573f3c37dc9742190c5f5ed68
gerg <gregross.dev@gmail.com>
2021-03-02 00:35:01 -0500

    fix: add return statement

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -2,8 +2,10 @@ import React from "react"
 import {PaginationWrapper, PaginationElement} from "../elements"
 
 export const Pagination = ({isFirst, isLast, prevPage, nextPage}) => {
-  <PaginationWrapper isFirst={isFirst} isLast={isLast}>
-    <PaginationElement to={prevPage}>Previous Page</PaginationElement>
-    <PaginationElement to={nextPage}>Next Page</PaginationElement>
-  </PaginationWrapper>
+  return (
+    <PaginationWrapper isFirst={isFirst} isLast={isLast}>
+      <PaginationElement to={prevPage}>Previous Page</PaginationElement>
+      <PaginationElement to={nextPage}>Next Page</PaginationElement>
+    </PaginationWrapper>
+  )
 }

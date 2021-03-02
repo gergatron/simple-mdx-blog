af2d5526717c33331b6d18dd3fafa8e3ff461ee5
gerg <gregross.dev@gmail.com>
2021-03-02 13:19:53 -0500

    createPages

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,48 +1,54 @@
-// exports.createPages = async function({actions, graphql}) {
-//   const {data} = await graphql`
-//     query {
-//       allMdx(sort: {fields: frontmatter___date, order: DESC}) {
-//         edges {
-//           node {
-//             frontmatter {
-//               slug
-//             }
-//             id
-//           }
-//         }
-//       }
-//     }
-//   `
+exports.createPages = async function({actions, graphql}) {
+  const {data} = await graphql(`
+    query {
+      allMdx(sort: {fields: frontmatter___date, order: DESC}) {
+        edges {
+          node {
+            frontmatter {
+              slug
+            }
+            id
+          }
+        }
+      }
+    }
+  `)
   
-//   // Create paginated pages for posts
-//   const postPerPage = 3
+  // Create paginated pages for posts
+  const postPerPage = 3
 
-//   const numPages = Math.ceil(data.allMdx.edges.length / postPerPage)
+  const numPages = Math.ceil(data.allMdx.edges.length / postPerPage)
   
-//   Array.from({ length: numPages }).forEach((_, i) => {
-//     actions.createPages({
-//       path: i === 0 ? `/` : `/${i + 1}`,
-//       component: require.resolve("./src/templates/allPosts.js"),
-//       context: {
-//         limit: postPerPage,
-//         skip: i * postPerPage,
-//         numPages,
-//         currentPage: i + 1, 
-//       }
-//     })
-//   })
-//   // ! End: Create paginated pages for posts
+  Array.from({ length: numPages }).forEach((_, i) => {
+    actions.createPage({
+      path: i === 0 ? `/` : `/${i + 1}`,
+      component: require.resolve("./src/templates/allPosts.js"),
+      context: {
+        limit: postPerPage,
+        skip: i * postPerPage,
+        numPages,
+        currentPage: i + 1, 
+      },
+    })
+  })
+  // ! End: Create paginated pages for posts
 
-//   // Create Single Blog Posts
-//   data.allMdx.edges.forEach(edge=> {
-//     const slug = edge.node.frontmatter.slug
-//     const id = edge.node.id
-//     actions.createPages({
-//       path: slug,
-//       component: require.resolve(`./src/templates/singlePost.js`),
-//       context: {id},
-//     })
-//   })
+  
 
 
-// }
+}
+
+{/*
+// Create Single Blog Posts
+data.allMdx.edges.forEach(edge=> {
+  const slug = edge.node.frontmatter.slug
+  const id = edge.node.id
+  actions.createPage({
+    path: slug,
+    component: require.resolve(`./src/templates/singlePost.js`),
+    context: {id},
+  })
+})
+
+
+*/}
